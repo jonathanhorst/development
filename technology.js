9532ae13f16434f81314d09954e4db9a662f2050
Jonathan Horst <jonathanhorst@users.noreply.github.com>
2023-12-08 11:34:50 -0600

    1.0.8.1

--- technology.js
+++ technology.js
@@ -1,44 +1,38 @@
 $(document).ready(function() {
 
+    let mm = gsap.matchMedia();
 
+    mm.add("(min-width: 480px)", () => {
 
-  let mm = gsap.matchMedia();
+        // Initialize ScrollTrigger
+        gsap.registerPlugin(ScrollTrigger);
 
-  mm.add("(min-width: 480px)", () => {
+        // Get the elements
+        const $quoteBackground = $("[th-element='technology-quote-background']");
+        const $quoteComponent = $("[th-element='technology-quote-component']");
 
+        const rootStyles = getComputedStyle(document.documentElement);
+        const paddingValue = rootStyles.getPropertyValue('--container-padding').trim();
+        const borderRadiusValue = rootStyles.getPropertyValue('--border-radius-large').trim();
 
+        // Create the animation
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: $quoteComponent,
+                start: "bottom bottom",
+            },
+        });
 
-    // Initialize ScrollTrigger
-    gsap.registerPlugin(ScrollTrigger);
+        // Add the .to animation
+        tl.to($quoteBackground, {
+            top: paddingValue,
+            left: paddingValue,
+            bottom: paddingValue,
+            right: paddingValue,
+            borderRadius: borderRadiusValue,
+            duration: 1,
+            ease: "power2.out",
+        });
 
-    // Get the elements
-    const $quoteBackground = $("[th-element='technology-quote-background']");
-    const $quoteComponent = $("[th-element='technology-quote-component']");
-
-    const rootStyles = getComputedStyle(document.documentElement);
-    const paddingValue = rootStyles.getPropertyValue('--container-padding').trim();
-    const borderRadiusValue = rootStyles.getPropertyValue('--border-radius-large').trim();
-
-    // Create the animation
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: $quoteComponent,
-        start: "bottom bottom",
-      },
-    });
-
-    // Add the .to animation
-    tl.to($quoteBackground, {
-      top: paddingValue,
-      left: paddingValue,
-      bottom: paddingValue,
-      right: paddingValue,
-      borderRadius: borderRadiusValue,
-      duration: 1,
-      ease: "power2.out",
-    });
-
-
-
-  })
+    })
 });
